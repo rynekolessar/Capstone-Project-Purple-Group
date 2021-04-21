9182eb032a6f431887fd47f835946659c50fccdf
rynekolessar <ryne.kolessar@gmail.com>
2021-04-21 14:50:33 -0500

    added games schema

---
+++ models/Game.js
@@ -0,0 +1,32 @@
+// models/Game.js
+
+const mongoose = require('mongoose');
+
+const GameSchema = new monoose.Schema({
+	title: {
+		type: String,
+		required: true
+	},
+	platform: { // PlayStation, XBox, PC, etc..
+		type: String,
+		required: true
+	},
+	genre: { // FPS, RPG, Horror, etc...
+		type: String
+	},
+	release_date: {
+		type: Date
+	},
+	description: {
+		type: String
+	},
+	game_studio: {
+		type: String
+	},
+	updated_date: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+module.exports = Game = mongoose.model('game', GameSchema);
