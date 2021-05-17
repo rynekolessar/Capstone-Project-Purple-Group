defdc0f6c2d1a4898be5d8d47fb1bf0d4f1828e4
rynekolessar <ryne.kolessar@gmail.com>
2021-05-17 17:52:02 -0500

    updated review controller and routes

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,8 +1,14 @@
-module.exports.setGameUserIds = (req, res, next) => {
-  if (!req.body.game) req.body.game = req.params.game;
+const Review = require('../models/Review');
+const factory = require('./handlerFactory');
+
+module.exports.setTourUserIds = (req, res, next) => {
+  if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
   next();
 };
 
-// TODO: review controller methods
-// module.exports.createReview = (req, res)
+module.exports.getAllReviews = factory.getAll(Review);
+module.exports.getReview = factory.getOne(Review);
+module.exports.createReview = factory.createOne(Review);
+module.exports.updateReview = factory.updateOne(Review);
+module.exports.deleteReview = factory.deleteOne(Review);
--- routes/api/reviewRoutes.js
+++ routes/api/reviewRoutes.js
@@ -4,14 +4,28 @@ const authController = require('../../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
-/* TODO: reviewRoutes
+// Protect all routes (below this point)
+router.use(authController.protect);
+
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
-    authController.requireSignin,
-    reviewController.setGameUserIds,
+    authController.restrictTo('user'),
+    reviewController.setTourUserIds,
     reviewController.createReview
   );
-*/
+
+router
+  .route('/:id')
+  .get(reviewController.getReview)
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview
+  );
+
+module.exports = router;
